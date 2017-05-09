419c0da77bbc6080c9fa434529a90e7e4f4a2078
ricardocerto16 <a75315@alunos.uminho.pt>
2017-05-09 16:11:36 +0100

    guiao 6 ex2

---
+++ Guiao6/ex2Server.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+
+
+
+	return 0;
+}
